dc06e9d68c2b3eb4ca1bd0f9faf975cc9b486b62
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-11-23 18:07:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+    .then(json => renderBooks(json)); 
 }
 
 function renderBooks(books) {
@@ -7,7 +9,11 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
+    const h3 = document.createElement('h3')
+    h3.innerHTML = book.numberOfPages
+
     main.appendChild(h2)
+    main.appendChild(h3)
   })
 }
 
