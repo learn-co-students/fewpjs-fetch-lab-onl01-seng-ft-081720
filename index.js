b511804f18b174906875955f5bcf884e38fa76ab
botto54 <bryanlbottorff@gmail.com>
2020-12-07 03:48:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main')
+  var main = document.querySelector('main')
   books.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = book.name
